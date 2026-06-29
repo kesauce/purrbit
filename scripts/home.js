248c0570160ded216a708794fcf4cb35021b337b
KESPC <alexismanosca@gmail.com>
2026-06-29 16:03:43 +1200

    add name saving feature

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,5 @@
+import Cat from '../Cat.js';
+
 // ────── Initialise Cat ──────
 async function initialiseGame() {
 	// Check if there's a saved state in the Chrome storage
@@ -6,8 +8,13 @@ async function initialiseGame() {
 	// Create a new cat if it doesn't exit
 	if (!cat) {
 		// Navigate to the create page
-		window.location.href = 'create.html';
+		window.location.href = "create.html";
+		return;
 	}
+
+	// Rebuilt the Cat object
+	const catInstance = Object.assign(new Cat(), cat);
+	document.getElementById("cat-name").textContent = catInstance.getName();
 }
 
 initialiseGame();
