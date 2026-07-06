1e6d1a240a48fc724c5c6ac68581f30b98faff39
KESPC <alexismanosca@gmail.com>
2026-07-06 21:13:29 +1200

    add volume change

--- scripts/audio.js
+++ scripts/audio.js
@@ -10,19 +10,18 @@ async function loadAudio(url) {
 }
 
 let source;
+const gainNode = audioContext.createGain();
 
 // Plays the music within the audio context
 async function startMusic() {
 	// Set the music based on the screen
 	let bgmPath = "../assets/bgm/bgm-marimba.wav";
-	if (window.location.href.includes("create.html")){
-		bgmPath ="../assets/bgm/bgm-piano.wav";
-	}
-	else if (window.location.href.includes("groom.html")){
-		bgmPath ="../assets/bgm/bgm-strings.wav";
-	}
-	else if (window.location.href.includes("settings.html")){
-		bgmPath = "../assets/bgm/bgm-wind.wav"
+	if (window.location.href.includes("create.html")) {
+		bgmPath = "../assets/bgm/bgm-piano.wav";
+	} else if (window.location.href.includes("groom.html")) {
+		bgmPath = "../assets/bgm/bgm-strings.wav";
+	} else if (window.location.href.includes("settings.html")) {
+		bgmPath = "../assets/bgm/bgm-wind.wav";
 	}
 
 	const url = chrome.runtime.getURL(bgmPath);
@@ -32,8 +31,26 @@ async function startMusic() {
 	source = audioContext.createBufferSource();
 	source.buffer = buffer;
 	source.loop = true;
-	source.connect(audioContext.destination);
+
+	// Connect volume
+	source.connect(gainNode);
+	gainNode.connect(audioContext.destination);
+
+	// Check if there's user preference for volume
+	const { volume } = await chrome.storage.local.get("volume");
+	gainNode.gain.value = volume ?? 1;
+
 	source.start(0);
 }
 
 startMusic();
+
+// Only get volume control if at settings page
+if (window.location.href.includes("settings.html")) {
+	document.getElementById("volume").addEventListener("input", async (e) => {
+		gainNode.gain.value = e.target.value;
+		
+		// Set volume preference
+		await chrome.storage.local.set({ volume: e.target.value });
+	});
+}
